396f094bcf0daa16c81791830110e64388959343
Andrew <lackieandrew@yahoo.com>
2022-09-23 01:02:23 -0700

    added hacking startpage

---
+++ hacking/terminal.js
@@ -0,0 +1,17 @@
+function updateTime(){
+    var currentTime = new Date()
+    var hours = currentTime.getHours()
+    var minutes = currentTime.getMinutes()
+    if (minutes < 10){
+        minutes = "0" + minutes
+    }
+    var t_str = hours + ":" + minutes + " ";
+    if(hours > 11){
+        t_str += "PM";
+    } else {
+        t_str += "AM";
+    }
+    document.getElementById('time_span').innerHTML = t_str;
+}
+setInterval(updateTime(), 1000);
+
